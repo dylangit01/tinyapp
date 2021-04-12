9918b2e62373e7f02fbde582a49131faccfb58e7
Dylan <dcren2010@gmail.com>
2021-04-12 16:25:34 -0400

    app redirect page built for shorURL to longURL

--- express_server.js
+++ express_server.js
@@ -9,18 +9,30 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
+  b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
+  // '4jYxmV': 'https://ca.yahoo.com/',
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello!');
+// });
 
-app.get('/hello', (req, res) => {
-  const templateVars = { greeting: 'Hello World!!!!!!' };
-  res.render('hello_world', templateVars);
-});
+// app.get('/hello', (req, res) => {
+//   const templateVars = { greeting: 'Hello World!!!!!!' };
+//   res.render('hello_world', templateVars);
+// });
+
+const generateRandomString = () => {
+  const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+  for (let i = 0; i <= 5; i++) {
+    let randomNum = Math.floor(Math.random() * stringList.length);
+    let randomLetter = stringList[randomNum];
+    randomString += randomLetter;
+  }
+  return randomString;
+};
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -31,16 +43,27 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// Create new url:
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
-// Create new url:
-app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('OK')
-})
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  try {
+    res.redirect(longURL)
+  } catch (error) {
+    res.status(404).json({ msg: error.message });
+  }
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
@@ -51,13 +74,5 @@ app.listen(PORT, () => {
 });
 
 
-const generateRandomString = () => {
-  const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let randomString = '';
-  for (let i = 0; i <= 6; i++) {
-    let randomNum = Math.floor(Math.random() * stringList.length);
-    let randomLetter = stringList[randomNum];
-    randomString += randomLetter;
-  }
-  return randomString;
-};
+
+
