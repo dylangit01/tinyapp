c7073578b930035e8df6f9328b1570417afb2db4
Dylan <dcren2010@gmail.com>
2021-04-12 13:52:40 -0400

    urls_new built for creating new url

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
+  b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
@@ -13,30 +13,28 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
-
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render('urls_show', templateVars)
-})
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get('/hello', (req, res) => {
   const templateVars = { greeting: 'Hello World!!!!!!' };
   res.render('hello_world', templateVars);
 });
 
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n');
-// });
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
