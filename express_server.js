3deffc31c8aec2bd74e137930abf99a923c11fff
Dylan <dcren2010@gmail.com>
2021-04-12 14:39:59 -0400

    post form created, created input api through POST and created a route to handle the post request

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// for body-parser:
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  b2xVn2: 'http://www.lighthouselabs.ca',
+  'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
@@ -32,6 +36,12 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+// Create new url:
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK')
+})
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
@@ -39,3 +49,15 @@ app.get('/urls.json', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+const generateRandomString = () => {
+  const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+  for (let i = 0; i <= 6; i++) {
+    let randomNum = Math.floor(Math.random() * stringList.length);
+    let randomLetter = stringList[randomNum];
+    randomString += randomLetter;
+  }
+  return randomString;
+};
