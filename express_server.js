5d8672b67eb807755843e0521e9b74a7a31df1c4
Dylan <dcren2010@gmail.com>
2021-04-12 10:08:16 -0400

    urls port created

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set('veiw engine', 'ejs');
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -13,15 +13,18 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 app.get('/urls.json', (req, res) => {
-	res.json(urlDatabase)
-})
+  res.json(urlDatabase);
+});
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
