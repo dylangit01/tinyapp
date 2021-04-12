f49b19753c66e3a50a17e751656dd3676ee8b027
Dylan <dcren2010@gmail.com>
2021-04-12 10:44:22 -0400

    hello url updated & hello_world view added

--- express_server.js
+++ express_server.js
@@ -23,8 +23,15 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+  const templateVars = { greeting: 'Hello World!' };
+  res.render('hello_world', templateVars);
 });
+
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
